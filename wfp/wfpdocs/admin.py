3ca27c3246734d48a984eb4be80726ef5b7099d4
capooti <pcorti@gmail.com>
2014-05-15 15:23:25 +0200

    The EmailAddressAdmin class is now provided in geonode-user-accounts latest version.

--- wfp/wfpdocs/admin.py
+++ wfp/wfpdocs/admin.py
@@ -2,13 +2,6 @@ from django.contrib import admin
 from wfp.wfpdocs.models import WFPDocument, Category
 from account.models import EmailAddress
 
-# we keep here it for now
-class EmailAddressAdmin(admin.ModelAdmin):
-    model = EmailAddress
-    list_display = ('user', 'email', 'verified')
-    list_filter = ('verified',)
-    #search_fields = ('name', 'workspace',)
-
 class WFPDocumentAdmin(admin.ModelAdmin):
     list_display = ('document', 'get_date', 'get_date_type', 'get_regions', 
         'source', 'get_categories', 'orientation', 'page_format', )
@@ -22,4 +15,3 @@ class CategoryAdmin(admin.ModelAdmin):
         
 admin.site.register(WFPDocument, WFPDocumentAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(EmailAddress, EmailAddressAdmin)
