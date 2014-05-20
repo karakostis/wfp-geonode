d1055c1a8c7098dfaaba708d8823d67cdb19dd19
CORTI Paolo <paolo.corti@wfp.org>
2014-05-20 12:41:09 +0200

    Closing registrations.

--- wfp/local_settings.py
+++ wfp/local_settings.py
@@ -164,7 +164,7 @@ EMAIL_USE_TLS = True
 THEME_ACCOUNT_CONTACT_EMAIL = 'wfp.geonode@gmail.com'
 
 # Account
-REGISTRATION_OPEN = True
+REGISTRATION_OPEN = False
 # set to False this if you want only invited users to be able to register
 ACCOUNT_OPEN_SIGNUP = False
 ACCOUNT_SIGNUP_REDIRECT_URL = 'profile_edit'
