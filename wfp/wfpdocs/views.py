4f43469090ca672d371eab507a02ca2878faf700
capooti <pcorti@gmail.com>
2014-05-22 11:26:18 +0200

    Added an is_public field.

--- wfp/wfpdocs/views.py
+++ wfp/wfpdocs/views.py
@@ -2,7 +2,8 @@ import json, os
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext
+from django.template import RequestContext, loader
+from django.utils.translation import ugettext as _
 from django.conf import settings
 from geonode.documents.models import Document
 from django.contrib.contenttypes.models import ContentType
