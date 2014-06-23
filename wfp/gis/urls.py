d1a788034f2b0fac159eebbcf1677ce758cc0836
CORTI Paolo <paolo.corti@wfp.org>
2014-06-23 11:24:58 +0200

    Added the gis employees map.

--- wfp/gis/urls.py
+++ wfp/gis/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url, include
+from django.views.generic import TemplateView
 
 from tastypie.api import Api
 
@@ -11,4 +12,5 @@ v1_api.register(EmployeeResource())
 urlpatterns = patterns(
     'wfp.gis.views',
     url(r'^api/', include(v1_api.urls)),
+    url(r'^employees-map/$', TemplateView.as_view(template_name='gis/employees-map.html')),
 )
