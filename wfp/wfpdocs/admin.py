ec1cc6ff4c411992a03807e370c7f44159bcbc67
capooti <pcorti@gmail.com>
2014-07-24 10:41:34 +0200

    Sorting wfpdocs by date, and catching the error when document is not existing for any reason.

--- wfp/wfpdocs/admin.py
+++ wfp/wfpdocs/admin.py
@@ -3,8 +3,8 @@ from wfp.wfpdocs.models import WFPDocument, Category
 from account.models import EmailAddress
 
 class WFPDocumentAdmin(admin.ModelAdmin):
-    list_display = ('document', 'get_date', 'get_date_type', 'get_regions', 
-        'source', 'get_categories', 'orientation', 'page_format', )
+    list_display = ('document', 'get_date', 'get_date_type', 'date_updated',
+        'get_regions', 'source', 'get_categories', 'orientation', 'page_format', )
     list_display_links = ('document',)
     list_filter  = ('orientation', 'page_format', 'categories' )
     search_fields = ('document__title',)
