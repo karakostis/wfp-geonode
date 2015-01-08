e95b9e152b4d726e3584af8623f35952743e22ba
CORTI Paolo <paolo.corti@wfp.org>
2015-01-08 15:56:44 +0100

    Setting static map publication time to now

--- wfp/wfpdocs/forms.py
+++ wfp/wfpdocs/forms.py
@@ -9,10 +9,11 @@ from django.contrib.contenttypes.models import ContentType
 
 class WFPDocumentForm(forms.ModelForm):
     publication_date = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
-    publication_date.widget.widgets[0].attrs = {'class':'datepicker', 'data-date-format': 'yyyy-mm-dd', 'value': str(datetime.date.today())}
-    publication_date.widget.widgets[1].attrs = {"class":"time", 'value':'00:00:00'}
-
-    source = forms.CharField()
+    publication_date.widget.widgets[0].attrs = {'class':'datepicker',
+        'data-date-format': 'yyyy-mm-dd', 'value': str(datetime.date.today())}
+    publication_date.widget.widgets[1].attrs = {"class":"time",
+        'value': datetime.datetime.now().strftime('%H:%M:%S')}
+    source = forms.CharField(required=False)
     orientation = forms.ChoiceField(WFPDocument.ORIENTATION_CHOICES)
     page_format = forms.ChoiceField(WFPDocument.FORMAT_CHOICES)
     categories = forms.ModelMultipleChoiceField(Category.objects.all(), required=False)
