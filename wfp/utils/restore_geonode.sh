5532dcc92a7e001c794c8a2bf8a14fe83a0baf9f
CORTI Paolo <paolo.corti@wfp.org>
2015-03-20 10:03:16 +0100

    Now the restore geonode utility can be run in differet steps

--- wfp/utils/restore_geonode.sh
+++ wfp/utils/restore_geonode.sh
@@ -6,49 +6,66 @@
 # read configuration
 # we need to have a gnadmin postgres user in place, with same password as in production
 CWD=$(pwd)
-DATE="20150311"
-BACKUP_DIR="/home/capooti/backup/geonode/backup_tar_gz"
+DATE="20150315"
+BACKUP_DIR="/home/capooti/backup/geonode"
 VEDIR="/home/capooti/git/codeassist/wfp-geonode/env"
 
 cd $BACKUP_DIR
 
 # remove old backup files and scp new ones
-rm $BACKUP_DIR/$DATE
-scp -r capooti@thebeast:/gis/backup/data/$DATE $BACKUP_DIR
+function dowload_backup {
+    rm $BACKUP_DIR/$DATE
+    scp -r capooti@thebeast:/gis/backup/data/sdi/$DATE $BACKUP_DIR
+}
 
 # restore gn_django
-DB=sdi_django
-DUMP=sdi_django.sql
-tar -xvf $BACKUP_DIR/$DATE/sdi/postgres.tar.gz $DUMP
-psql -U gnadmin -c "DROP DATABASE $DB;" postgres
-psql -U gnadmin -c "CREATE DATABASE $DB OWNER gnadmin;" postgres
-psql -U gnadmin -c "CREATE EXTENSION POSTGIS;" $DB
-psql -U gnadmin -f /usr/share/postgresql/9.2/contrib/postgis-2.1/legacy.sql $DB
-psql -U gnadmin $DB < $BACKUP_DIR/$DUMP 2> error_$DB.log
+function restore_django {
+    DB=sdi_django
+    DUMP=sdi_django.sql
+    tar -xvf $BACKUP_DIR/$DATE/postgres.tar.gz $DUMP
+    psql -U gnadmin -c "DROP DATABASE $DB;" postgres
+    psql -U gnadmin -c "CREATE DATABASE $DB OWNER gnadmin;" postgres
+    psql -U gnadmin -c "CREATE EXTENSION POSTGIS;" $DB
+    psql -U gnadmin -f /usr/share/postgresql/9.2/contrib/postgis-2.1/legacy.sql $DB
+    psql -U gnadmin $DB < $BACKUP_DIR/$DUMP 2> error_$DB.log
+}
 
 # restore gn_uploads
-DB=sdi_uploads
-DUMP=sdi_uploads.sql
-tar -xvf $BACKUP_DIR/$DATE/sdi/postgres.tar.gz $DUMP -C $BACKUP_DIR
-psql -U gnadmin -c "DROP DATABASE $DB;" postgres
-psql -U gnadmin -c "CREATE DATABASE $DB OWNER gnadmin;" postgres
-psql -U gnadmin -c "CREATE EXTENSION POSTGIS;" $DB
-psql -U gnadmin -f /usr/share/postgresql/9.2/contrib/postgis-2.1/legacy.sql $DB
-psql -U gnadmin $DB < $BACKUP_DIR/$DUMP 2> error_$DB.log
+function restore_uploads {
+    DB=sdi_uploads
+    DUMP=sdi_uploads.sql
+    tar -xvf $BACKUP_DIR/$DATE/postgres.tar.gz $DUMP -C $BACKUP_DIR
+    psql -U gnadmin -c "DROP DATABASE $DB;" postgres
+    psql -U gnadmin -c "CREATE DATABASE $DB OWNER gnadmin;" postgres
+    psql -U gnadmin -c "CREATE EXTENSION POSTGIS;" $DB
+    psql -U gnadmin -f /usr/share/postgresql/9.2/contrib/postgis-2.1/legacy.sql $DB
+    psql -U gnadmin $DB < $BACKUP_DIR/$DUMP 2> error_$DB.log
+}
 
 # restore geoserver
-GEOSERVER_DATA_DIRECTORY=/home/capooti/git/codeassist/geonode/geoserver
-rm -rf $GEOSERVER_DATA_DIRECTORY/data
-cd /tmp
-rm /tmp/geoserver
-tar -xvf $BACKUP_DIR/$DATE/sdi/geoserver.tar.gz
-mv /tmp/geoserver $GEOSERVER_DATA_DIRECTORY/data
-cd $CWD
-sed -i 's/geonode.wfp.org/localhost:8000/g' $GEOSERVER_DATA_DIRECTORY/data/security/auth/geonodeAuthProvider/config.xml
+function restore_geoserver {
+    GEOSERVER_DATA_DIRECTORY=/home/capooti/git/codeassist/geonode/geoserver
+    rm -rf $GEOSERVER_DATA_DIRECTORY/data
+    cd /tmp
+    rm /tmp/geoserver
+    tar -xvf $BACKUP_DIR/$DATE/geoserver.tar.gz
+    mv /tmp/geoserver $GEOSERVER_DATA_DIRECTORY/data
+    cd $CWD
+    sed -i 's/geonode.wfp.org/localhost:8000/g' $GEOSERVER_DATA_DIRECTORY/data/security/auth/geonodeAuthProvider/config.xml
+}
 
 # set site name
-. $VEDIR/bin/activate
-./manage.py shell < wfp/utils/set_sitedomain.py
+function set_site_name {
+    . $VEDIR/bin/activate
+    ./manage.py shell < wfp/utils/set_sitedomain.py
+}
+
+# main
+dowload_backup
+restore_django
+restore_uploads
+restore_geoserver
+set_site_name
 
 # migrations
 #./manage.py migrate base --fake 0002
