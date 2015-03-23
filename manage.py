ec8789582317be4a9e6846bda7496d2db3d795d5
CORTI Paolo <paolo.corti@wfp.org>
2015-03-23 11:42:49 +0100

    Testing jenkins with settings explicitely set to testing

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wfp.settings.testing')
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wfp.settings")
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
