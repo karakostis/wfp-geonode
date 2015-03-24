93228c46c867b211f1a0e532e5fcb8ec51a6dc1b
CORTI Paolo <paolo.corti@wfp.org>
2015-03-24 14:32:57 +0100

    Updated testing settings to new wallet configuration

--- wfp/settings/testing.py
+++ wfp/settings/testing.py
@@ -1,23 +1,24 @@
 from .default import *
 
-DEBUG = TEMPLATE_DEBUG = True
+DEBUG = True
+TEMPLATE_DEBUG = True
 DEBUG_STATIC = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': GEONODE_DJANGO_DB,
-        'USER': GEONODE_USER,
-        'PASSWORD': GEONODE_PWD,
+        'NAME': wallet.DATABASES.default.NAME,
+        'USER': wallet.DATABASES.default.USER,
+        'PASSWORD': wallet.DATABASES.default.PASSWORD,
         'HOST': '10.11.40.227',
         'PORT': '5432',
     },
     # vector datastore for uploads
     'uploaded' : {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': GEONODE_POSTGIS_DB,
-        'USER' : GEONODE_USER,
-        'PASSWORD' : GEONODE_PWD,
+        'NAME': wallet.DATABASES.uploaded.NAME,
+        'USER' : wallet.DATABASES.uploaded.USER,
+        'PASSWORD' : wallet.DATABASES.uploaded.PASSWORD,
         'HOST' : '10.11.40.227',
         'PORT' : '5432',
     }
