3c18ab8062f137dcf681e49692389be72786a72a
Paolo Corti <paolo.corti@wfp.org>
2015-06-16 16:47:56 +0200

    Removing OGC settings from local setting

--- wfp/settings/_geonode24.py
+++ wfp/settings/_geonode24.py
@@ -24,28 +24,5 @@ DATABASES = {
     }
 }
 
-OGC_SERVER = {
-    'default': {
-        'BACKEND': 'geonode.geoserver',
-        'LOCATION': 'http://localhost:8080/geoserver/',
-        # PUBLIC_LOCATION needs to be kept like this because in dev mode
-        # the proxy won't work and the integration tests will fail
-        # the entire block has to be overridden in the local_settings
-        'PUBLIC_LOCATION': 'http://localhost:8080/geoserver/',
-        'USER': 'admin',
-        'PASSWORD': 'geoserver',
-        'MAPFISH_PRINT_ENABLED': True,
-        'PRINT_NG_ENABLED': True,
-        'GEONODE_SECURITY_ENABLED': True,
-        'GEOGIG_ENABLED': False,
-        'WMST_ENABLED': False,
-        'BACKEND_WRITE_ENABLED': True,
-        'WPS_ENABLED': False,
-        'LOG_FILE': '/home/capooti/git/codeassist/geonode/geoserver/data/logs/geoserver.log',
-        # Set to name of database in DATABASES dictionary to enable
-        'DATASTORE': '',  # 'datastore',
-        'TIMEOUT': 10  # number of seconds to allow for HTTP requests
-    }
-}
-
 MEDIA_ROOT = "/home/capooti/git/codeassist/wfp-geonode/www/uploaded"
+STATIC_ROOT="/home/capooti/git/codeassist/wfp-geonode/www/static/"
