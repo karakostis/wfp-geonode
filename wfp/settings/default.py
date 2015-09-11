0e0577f13ee5aa3a6e3ff20802b4787902e65bfe
Paolo Corti <paolo.corti@wfp.org>
2015-09-11 17:57:31 +0200

    We do not display statistics in attributes in layer details page as per #GEONODE-40

--- wfp/settings/default.py
+++ wfp/settings/default.py
@@ -159,7 +159,7 @@ OGC_SERVER = {
         'GEOGIT_ENABLED' : False,
         'WMST_ENABLED' : False,
         'BACKEND_WRITE_ENABLED': True,
-        'WPS_ENABLED' : True,
+        'WPS_ENABLED' : False,
         'LOG_FILE': '%s/geoserver/data/logs/geoserver.log' % os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir)),
         # Set to name of database in DATABASES dictionary to enable
         'DATASTORE': 'uploaded', #'datastore',
