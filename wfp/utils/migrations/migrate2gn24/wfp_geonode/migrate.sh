a27c0d1c18e240b7572d10d18cd00d7d364ecb1a
Paolo Corti <paolo.corti@wfp.org>
2015-09-22 11:44:01 +0200

    We need to migrate some base models as per #GEONODE-50

--- wfp/utils/migrations/migrate2gn24/wfp_geonode/migrate.sh
+++ wfp/utils/migrations/migrate2gn24/wfp_geonode/migrate.sh
@@ -1,5 +1,8 @@
+# to run on the source database before geonode migrate.sh
 # 0. only WFP: run wfp/schema_changes.sql
 # 1. run wfp/update_esri_gn_store.sql
+# 2. run wfp/set_owner_when_is_null.sql
+python $DIR/migrate_base_metadata.py
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
