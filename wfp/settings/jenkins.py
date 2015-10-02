610b843c4139552089a87a138e07335ec7090e5c
Paolo Corti <paolo.corti@wfp.org>
2015-10-02 12:17:34 +0200

    Now jenkins postgres supports postgis, so no need to use a different server

--- wfp/settings/jenkins.py
+++ wfp/settings/jenkins.py
@@ -1,11 +1,3 @@
 from .testing import *  # noqa
 
 GEOS_LIBRARY_PATH = '/opt/wfp_jenkins_instances/wfp_jenkins104/lib/libgeos_c.so'
-
-DATABASES['default']['USER'] = os.getenv('test_user', 'jenkins')
-DATABASES['default']['PASSWORD'] = os.getenv('test_pwd', 'secret')
-DATABASES['default']['HOST'] = '10.11.40.227'
-
-DATABASES['uploaded']['USER'] = os.getenv('test_user', 'jenkins')
-DATABASES['uploaded']['PASSWORD'] = os.getenv('test_pwd', 'secret')
-DATABASES['uploaded']['HOST'] = '10.11.40.227'
