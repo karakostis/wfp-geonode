2f578d894c9782fc3091eceda1e4be6500fec00b
Paolo Corti <paolo.corti@wfp.org>
2015-10-02 13:59:13 +0200

    Passing credentials to jenkins db server

--- wfp/settings/jenkins.py
+++ wfp/settings/jenkins.py
@@ -2,3 +2,6 @@ from .testing import *  # noqa
 
 GEOS_LIBRARY_PATH = '/opt/wfp_jenkins_instances/wfp_jenkins104/lib/libgeos_c.so'
 POSTGIS_VERSION = (2, 0, 7)
+
+DATABASES['default']['USER'] = 'postgres'
+DATABASES['uploaded']['USER'] = 'postgres'
