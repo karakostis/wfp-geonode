62c54a27f98d78d58e37beebf13e45db63d596a9
Paolo Corti <paolo.corti@wfp.org>
2015-10-23 17:43:31 +0200

    Making flake8 happy

--- wfp/settings/_capooti.py
+++ wfp/settings/_capooti.py
@@ -4,9 +4,9 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 DEBUG_STATIC = False
 
-#INSTALLED_APPS = INSTALLED_APPS + (
-#    'debug_toolbar',
-#)
+# INSTALLED_APPS = INSTALLED_APPS + (
+#     'debug_toolbar',
+# )
 
 MEDIA_ROOT = "/home/capooti/git/codeassist/wfp-geonode/www/uploaded"
 STATIC_ROOT = "/home/capooti/git/codeassist/wfp-geonode/www/static/"
