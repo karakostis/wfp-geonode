13cffbbe669f63ea2004585891ab4dfe128b49ef
dimitris.karakostis <dimitris.karakostis@wfp.org>
2017-10-03 09:19:34 +0200

    add https again..

--- wfp/settings/default.py
+++ wfp/settings/default.py
@@ -619,7 +619,7 @@ MAP_BASELAYERS = [{
     "type": "OpenLayers.Layer.OSM",
     #"source": {"ptype": "gxp_olsource"},
     #"type":"OpenLayers.Layer.XYZ",
-    #"args":[ "Open Street Map", ["http://a.tile.openstreetmap.org/${z}/${x}/${y}.png"],
+    #"args":[ "Open Street Map", ["https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"],
     #{"transitionEffect": "resize","attribution": "osm_attribution"}],
     "name": "mapnik",
     "visibility": True,
@@ -652,7 +652,7 @@ MAP_BASELAYERS = [{
 },{
     "source": {"ptype": "gxp_olsource"},
     "type":"OpenLayers.Layer.XYZ",
-    "args":[ "Humanitarian Openstreetmap", ["http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png"],
+    "args":[ "Humanitarian Openstreetmap", ["https://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png"],
     {"transitionEffect": "resize","attribution": "osm_attribution"}],
     "name":"HOT",
     "visibility": False,
@@ -663,7 +663,7 @@ MAP_BASELAYERS = [{
     "type": "OpenLayers.Layer.XYZ",
     "args": [
                 'Mapbox Dark',
-        ('http://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/${z}/'
+        ('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/${z}/'
          '${x}/${y}?access_token=%s') % (MAPBOX_ACCESS_TOKEN),
         {
             'transitionEffect': 'resize',
@@ -678,7 +678,7 @@ MAP_BASELAYERS = [{
     "type": "OpenLayers.Layer.XYZ",
     "args": [
                 'Mapbox Satellite',
-        ('http://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/${z}/'
+        ('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/${z}/'
          '${x}/${y}?access_token=%s') % (MAPBOX_ACCESS_TOKEN),
         {
             'transitionEffect': 'resize',
@@ -693,7 +693,7 @@ MAP_BASELAYERS = [{
     "type": "OpenLayers.Layer.XYZ",
     "args": [
                 'Mapbox Satellite Streets',
-        ('http://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/${z}/'
+        ('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/${z}/'
          '${x}/${y}?access_token=%s') % (MAPBOX_ACCESS_TOKEN),
         {
             'transitionEffect': 'resize',
@@ -714,23 +714,23 @@ SOCIAL_ORIGINS = [{
     "css_class":"email"
 }, {
     "label":"Facebook",
-    "url":"http://www.facebook.com/sharer.php?u={url}",
+    "url":"https://www.facebook.com/sharer.php?u={url}",
     "css_class":"fb"
 }, {
     "label":"Twitter",
-    "url":"http://twitter.com/share?url={url}",
+    "url":"https://twitter.com/share?url={url}",
     "css_class":"tw"
 }, {
     "label":"Google +",
-    "url":"http://plus.google.com/share?url={url}",
+    "url":"https://plus.google.com/share?url={url}",
     "css_class":"gp"
 }]
 
 #CKAN Query String Parameters names pulled from
-#http://github.com/ckan/ckan/blob/2052628c4a450078d58fb26bd6dc239f3cc68c3e/ckan/logic/action/create.py#L43
+#https://github.com/ckan/ckan/blob/2052628c4a450078d58fb26bd6dc239f3cc68c3e/ckan/logic/action/create.py#L43
 CKAN_ORIGINS = [{
     "label":"Humanitarian Data Exchange (HDX)",
-    "url":"http://data.hdx.rwlabs.org/dataset/new?title={name}&dataset_date={date}&notes={abstract}&caveats={caveats}",
+    "url":"https://data.hdx.rwlabs.org/dataset/new?title={name}&dataset_date={date}&notes={abstract}&caveats={caveats}",
     "css_class":"hdx"
 }]
 #SOCIAL_ORIGINS.extend(CKAN_ORIGINS)
@@ -813,17 +813,17 @@ LEAFLET_CONFIG = {
 
         # Stamen toner lite.
         ('OpenStreetMap',
-         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-         'Map tiles by <a href="http://openstreetmap.org">Stamen Design</a>, \
-         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
-         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
-         <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
+         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+         'Map tiles by <a href="https://openstreetmap.org">Stamen Design</a>, \
+         <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
+         <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, \
+         <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
          #('Watercolor',
-         # 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
-         # 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
-         # <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
-         # <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
-         # <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
+         # 'https://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
+         # 'Map tiles by <a href="https://stamen.com">Stamen Design</a>, \
+         # <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
+         # <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, \
+         # <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
     ],
     'PLUGINS': {
         'esri-leaflet': {
