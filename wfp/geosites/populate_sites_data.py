1e54cb05d167f927d03be1e3a854b472f348c648
dimitris.karakostis <dimitris.karakostis@wfp.org>
2017-10-10 14:49:19 +0200

    testing geosites

--- wfp/geosites/populate_sites_data.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.sites.models import Site
-
-
-def create_sites():
-    Site.objects.all().delete()
-    Site.objects.create(name='Master', domain="master.test.org")
-    Site.objects.create(name='Slave', domain="slave.test.org")
